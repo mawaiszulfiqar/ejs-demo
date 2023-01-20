7a261106f61c18b1e178370adaea90f93fbadd2c
mawaiszulfiqar <120576007+mawaiszulfiqar@users.noreply.github.com>
2023-01-20 21:42:23 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
 var express = require('express');
 var app = express();
-//var router = express.Router();
+
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
 // use res.render to load up an ejs view file
 
 // index page
-app.get('/index.html', async(req, res) => {
-  console.log("in home index");
-  res.render('pages/index.ejs');
+app.get('/', function(req, res) {
+  res.render('pages/index');
 });
-
 
 // about page
 app.get('/about', function(req, res) {
-  res.render('pages/about.ejs');
+  res.render('pages/about');
+});
 
-//router.get('/*.html', (req, res) => {
-//  res.send('About birds')
-//});
- 
 app.listen(8080);
 console.log('Server is listening on port 8080');
